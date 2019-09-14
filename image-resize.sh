dcecf5b2aa785bfda07a44dce80ff63b642747a9
Lai Zit Seng <lzs@zitseng.com>
2019-09-14 09:52:46 +0800

    Add command line arg parsing for max output file size

--- image-resize.sh
+++ image-resize.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
 
-filename=$1
-extension="${filename##*.}"
-filename="${filename%.*}"
+OPTIND=1
+maxsize=300
 
-convert $1 -define jpeg:extent=384kb -resize 2048x1365\> $filename-384.$extension
+do_help() {
+	command=`basename $0`
+	cat <<EOM
+Usage: $basename [options] files...
 
+    -s <size>   Resize to maximum output file output size of <size> kb (default 300)
+EOM
+}
+
+while getopts "h?s:" opt; do
+	case "$opt" in
+	h|\?)
+		do_help
+		exit 0
+		;;
+	s)
+		maxsize=$OPTARG
+		;;
+	esac
+done
+
+shift $((OPTIND-1))
+
+for filename in "$@"
+do
+	extension="${filename##*.}"
+	basename="${filename%.*}"
+	echo "Resizing $filename to $basename-$maxsize.$extension"
+	convert $filename -define jpeg:extent=${maxsize}kb -resize 2048x1365\> $basename-$maxsize.$extension
+done
+
+exit
