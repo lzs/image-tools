536642f8b1a67fadb9ac2267c658ac8e64a67cc3
Lai Zit Seng <lzs@zitseng.com>
2019-09-28 21:20:54 +0800

    Fix small formatting

--- image-shadows.sh
+++ image-shadows.sh
@@ -44,7 +44,8 @@ for i in $FILES; do
 	newfile="$filename-shadow-$$.$extension"
 	filelist+=($newfile)
 	echo Adding shadow to $i
-	convert $i \( +clone  -background gray  -shadow 90x20+20+20% \) +swap           -background white -layers merge +repage $newfile
+	convert $i \( +clone  -background gray  -shadow 90x20+20+20% \) +swap \
+		-background white -layers merge +repage $newfile
 done
 
 convert +append ${filelist[*]} ${OUTPUT%.*}-$$.${OUTPUT##*.}
